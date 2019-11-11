8674ee6531ba79011c2de746f44dd58db30157d0
Yanrishatum <krabanek@gmail.com>
2019-11-11 19:06:09 +0300

    Fix HLC compilation

--- compile-hlc.bat
+++ compile-hlc.bat
@@ -1,5 +1,5 @@
 @ECHO off
 haxe build-hlc.hxml -D ammer.hl.hlInclude=E:/HaxeToolkit/hl/include -D ammer.hl.hlLibrary=E:/HaxeToolkit/hl
 pushd bin\hlc
-cl /Ox fontgen.c -I . -I E:\HaxeToolkit\hl\include E:\HaxeToolkit\hl\libhl.lib ..\..\native\hl\ammer_msdfgen.hl.lib
+cl /Ox fontgen.c -I . -I E:\HaxeToolkit\hl\include E:\HaxeToolkit\hl\libhl.lib ..\..\native\hl\ammer_msdfgen_lib.hl.lib
 popd
