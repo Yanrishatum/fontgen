f27ec6f87080ad6a5e90e6dea72e306b5fe58440
artem pecheny <pecheny@gmail.com>
2021-06-23 11:37:32 +0300

    Fix build script.

--- compile-hlc.bat
+++ compile-hlc.bat
@@ -4,5 +4,5 @@ haxe build-hlc.hxml -D ammer.hl.hlInclude=%HLPATH%/include -D ammer.hl.hlLibrary
 pushd bin\hlc
 cl /Ox fontgen.c -I . -I %HLPATH%\include %HLPATH%\libhl.lib ..\..\native\hl\ammer_msdfgen_lib.hl.lib
 popd
-copy native\hl\ammer_msdfgen_lib.hl.dll bin\hlc
-copy native\msdfgen_lib.dll bin\hlc
+copy native\hl\ammer_msdfgen_lib.hl.dll bin\hlc\
+copy native\msdfgen_lib.dll bin\hlc\
